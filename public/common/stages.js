8ee18fbe8257d465d9ba6c56871e9da8db0744c0
gianprog3 <gian_martin_carlos@dlsu.edu.ph>
2025-11-19 16:16:57 +0800

    Added PC for each stage

--- public/common/stages.js
+++ public/common/stages.js
@@ -1,4 +1,4 @@
-const NOP = 0x00000013; //ADDI x0, x0, 0 basically does nothing.
+export const NOP = 0x00000013;
 
 export let IF = {
     IR: NOP,
@@ -11,23 +11,27 @@ export let ID = {
     B: 0x00000000,
     IMM: 0x00000000,
     IR: NOP,
-    NPC: 0x00000000
+    NPC: 0x00000000,
+    PC: 0x00000000
 };
 
 export let EX = {
     ALUOUTPUT: 0x00000000,
     IR: NOP,
     B: 0x00000000,
-    COND: 0
+    COND: 0,
+    PC: 0x00000000
 };
 
 export let MEM = {
     LMD: 0x00000000,
     IR: NOP,
     ALUOUTPUT: 0x00000000,
-    MEMALUOUTPUT: 0x00000000
+    MEMALUOUTPUT: 0x00000000,
+    PC: 0x00000000
 };
 
-export let WB ={
-    Rn: 0x00000000
+export let WB = {
+    Rn: 0x00000000,
+    PC: 0x00000000 // Optional, not used
 };
